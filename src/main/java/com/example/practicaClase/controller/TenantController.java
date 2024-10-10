180c7b765a6ff24d38f10853a9a0eba166569a1e
LautaroFavoni <lautarocarp4@hotmail.com>
2024-10-10 17:44:13 -0300

    nueva base de datos

--- src/main/java/com/example/practicaClase/controller/TenantController.java
+++ src/main/java/com/example/practicaClase/controller/TenantController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "**")
 @RequestMapping("/tenant")
 public class TenantController {
     @Autowired
